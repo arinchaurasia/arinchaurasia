4152ef17254105391f4ff13b795e2b3bcc429ebb
Arin Chaurasia <atulchaurasia39@gmail.com>
2025-11-05 06:23:02 +0000

    Sync LeetCode submission Runtime - 10 ms (45.77%), Memory - 7.6 MB (89.11%)

--- leetcode-solutions/1013-fibonacci-number/solution.cpp
+++ leetcode-solutions/1013-fibonacci-number/solution.cpp
@@ -1,67 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1) return n;
-        int c, a =0 ,b =1;
-        for(int i = 1;i<n;i++){
-            c=  a+b;
-            a = b;
-            b = c;
-        }
-        return c;
+        //base case
+        if(n == 0)
+        return 0;
+        if(n == 1)
+        return 1;
+
+        int ans = fib(n-1)+fib(n-2);
+        return ans;
+
     }
-
-
-
-    // Bottom Up
-
-    // int fib(int n) {
-    //     if(n<=1) return n;
-    //     vector<int>dp(n+1,-1);
-    //     dp[0]= 0;
-    //     dp[1] = 1;
-    //     for(int i = 2;i<=n;i++){
-    //         dp[i] = dp[i-1]+dp[i-2];
-    //     }
-    //     return dp[n];
-    // }
-
-
-    // recursion + memo
-    // int solve(int n, vector<int>& dp) {
-    //     if (n <= 1)
-    //         return n;
-    //     if (dp[n] != -1)
-    //         return dp[n];
-    //     return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
-    // }
-    // int fib(int n) {
-
-    //     if (n <= 1)
-    //         return n;
-
-    //     vector<int> dp(n + 1, -1);
-
-    //     return solve(n, dp);
-    // }
-
-
-    // int fib(int n) {
-    //     if(n<=1) return n;
-
-    //     return fib(n-1)+fib(n-2);
-    // }
-
-    // int fib(int n) {
-    //     if (n <= 1) return n;
-
-    //     vector<int> dp(n + 1, 0);
-    //     dp[0] = 0;
-    //     dp[1] = 1;
-    //     for (int i = 2; i <= n; ++i) {
-    //         dp[i] = dp[i - 1] + dp[i - 2];
-    //     }
-    //     return dp[n];
-    // }
 };
-
