0e2aa035df93a4ed1ddeb1894efea3aff282b39d
Arin Chaurasia <atulchaurasia39@gmail.com>
2025-11-10 09:06:14 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 8.1 MB (16.81%)

--- leetcode-solutions/1013-fibonacci-number/solution.cpp
+++ leetcode-solutions/1013-fibonacci-number/solution.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int fib(int n) {
+    int helper(int n,vector<int> &dp) {
         //base case
-        if(n == 0)
-        return 0;
-        if(n == 1)
-        return 1;
-
-        int ans = fib(n-1)+fib(n-2);
-        return ans;
-
+        if(n == 0 ||n == 1) return n;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = helper(n-1,dp)+helper(n-2,dp);
+    }
+    int fib(int n){
+        vector<int> dp(n+1,-1);
+        return helper(n,dp);
     }
 };
