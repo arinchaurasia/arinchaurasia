d9268cad29455a37b48dca16a920cf606e022eb9
Arin Chaurasia <atulchaurasia39@gmail.com>
2025-11-10 09:44:51 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 8.5 MB (53.55%)

---
+++ leetcode-solutions/0070-climbing-stairs/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int helper(int n, vector<int>& dp) {
+        // base cases
+        if (n < 0) return 0;
+        if (n == 0) return 1;
+        if (dp[n] != -1) return dp[n];
+        
+        dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
+
+        return dp[n];
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+        return helper(n, dp);
+    }
+};
+
