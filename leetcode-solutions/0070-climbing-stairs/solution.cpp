28db27d308cdb34ee070414cbdbd95f5ef97ede9
Arin Chaurasia <atulchaurasia39@gmail.com>
2025-11-10 09:44:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.5 MB (53.98%)

--- leetcode-solutions/0070-climbing-stairs/solution.cpp
+++ leetcode-solutions/0070-climbing-stairs/solution.cpp
@@ -1,69 +1,19 @@
 class Solution {
 public:
+    int helper(int n, vector<int>& dp) {
+        // base cases
+        if (n < 0) return 0;
+        if (n == 0) return 1;
+        if (dp[n] != -1) return dp[n];
+        
+        dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
 
-//Space Optimized
-int climbStairs(int n) {
-  
-    if(n==1 || n==2 || n==3) return n;
-    int c=3, a = 1, b = 2;
-     for (int i = 3; i <= n; i++) {
-            c = a + b;
-            a = b;
-            b = c;
-        }
+        return dp[n];
+    }
 
-    return c;
-
-}
-
-
-//  Bottom Up
-// int climbStairs(int n) {
-//     if(n==1 || n==2 || n==3) return n;
-//     vector<int>dp(n+1);
-//     dp[0] = 1;
-//     dp[1] = 1;
-//     dp[2] = 2;
-//     for(int i = 3;i<=n;i++){
-//         dp[i] = dp[i-1]+dp[i-2];
-//     }
-//     return dp[n];
-// }
-
-
-// recursion + memo or Top Down
-    // int climbStairs(int n) {
-    //     if(n<0) return 0;
-    //     if(n==0 ) return 1;
-    //     vector<int>dp(n+1,-1);
-
-    //     if(dp[n]!=-1) return dp[n];
-    //     return dp[n] = climbStairs(n-1)+climbStairs(n-2);
-
-    // }
-
-
-
-
-
-    // Brute Force
-    // int climbStairs(int n) {
-    //     if(n<0) return 0;
-    //     if(n==0 ) return 1;
-
-    //     return climbStairs(n-1)+climbStairs(n-2);
-
-    // }
-
-    // int climbStairs(int n) {
-    //     vector<int> dp(n + 1);
-    //     if (n == 1 || n == 2)
-    //         return n;
-    //     dp[1]=1;
-    //     dp[2]=2;
-    //     for (int i = 3; i <= n; i++) {
-    //         dp[i] = dp[i - 1] + dp[i - 2];
-    //     }
-    //     return dp[n];
-    // }
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+        return helper(n, dp);
+    }
 };
+
