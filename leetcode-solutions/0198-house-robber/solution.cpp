b76b9781bbc69d42b2d5bbb76f107c701057ef83
Arin Chaurasia <atulchaurasia39@gmail.com>
2025-11-11 10:39:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.7 MB (47.29%)

--- leetcode-solutions/0198-house-robber/solution.cpp
+++ leetcode-solutions/0198-house-robber/solution.cpp
@@ -1,54 +1,16 @@
 class Solution {
 public:
-    // Space Optimized
-
-    // Bottom Up
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1)
-            return nums[0];
-        vector<int> dp(n + 1, 0);
-        // dp[i] -> max stolen money till i house
-        dp[0] = 0;
-        dp[1] = nums[0];
-
-        for (int i = 2; i <= n; i++) {
-            int steal = nums[i - 1] + dp[i - 2];
-            int skip = dp[i - 1];
-            dp[i] = max(skip, steal);
+    int helper(vector<int> &nums, int ind,vector<int> &dp){
+        if(ind == nums.size() - 1){
+            return nums[ind];
+        } else if(ind >= nums.size()){
+            return 0;
         }
-        return dp[n];
+        if(dp[ind] != -1) return dp[ind];
+            return dp[ind] = max(helper(nums, ind+1, dp), nums[ind] + helper(nums, ind + 2, dp));
     }
-
-    // Recursion + memo
-    // int solve(vector<int>& nums, int i) {
-    //     int n = nums.size();
-    //     vector<int> dp(n + 1, -1);
-    //     if (i >= n)
-    //         return 0;
-    //     if (dp[i] != -1)
-    //         return dp[i];
-    //     int steal = nums[i] + solve(nums, i + 2);
-    //     int skip = solve(nums, i + 1);
-    //     return dp[i] = max(steal, skip);
-    // }
-
-    // int rob(vector<int>& nums) {
-    //     return solve(nums, 0);
-    // }
-
-    // Recursion
-    // int solve(vector<int>& nums,int i){
-    //     int n = nums.size();
-    //     if(i>=n) return 0;
-    //     int steal = nums[i]+solve( nums,i+2);
-    //     int skip = solve(nums,i+1);
-    //     return max(steal,skip);
-
-    // }
-
-    // int rob(vector<int>& nums){
-
-    //     return solve(nums,0);
-    // }
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size() + 1, -1);
+        return helper(nums, 0, dp);
+    }
 };
