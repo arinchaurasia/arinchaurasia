3e228bf955530702bae3b978744a848f7a921626
Arin Chaurasia <atulchaurasia39@gmail.com>
2025-11-13 10:51:38 +0000

    Sync LeetCode submission Runtime - 46 ms (15.60%), Memory - 27.6 MB (32.26%)

--- leetcode-solutions/1250-longest-common-subsequence/solution.cpp
+++ leetcode-solutions/1250-longest-common-subsequence/solution.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
+    int helper(string &s, string &t, int ind1, int ind2, vector<vector<int>>& dp) {
+        // base case
+        if (ind1 == s.length() || ind2 == t.length())
+            return 0;
+
+        if (dp[ind1][ind2] != -1)
+            return dp[ind1][ind2];
+
+        if (s[ind1] == t[ind2]) {
+            return 1 + helper(s, t, ind1 + 1, ind2 + 1, dp);
+        } 
+        else {
+            int skipS = helper(s, t, ind1 + 1, ind2, dp);
+            int skipT = helper(s, t, ind1, ind2 + 1, dp);
+            return dp[ind1][ind2] = max(skipS, skipT);
+        }
+    }
+
     int longestCommonSubsequence(string s, string t) {
         int n = s.length();
         int m = t.length();
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = m - 1; j >= 0; j--) {
-                if (s[i] == t[j]) {
-                    dp[i][j] = 1 + dp[i + 1][j + 1];
-                } else {
-                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
-                }
-            }
-        }
-
-        return dp[0][0];
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        
+        return helper(s, t, 0, 0, dp);
     }
 };
 
