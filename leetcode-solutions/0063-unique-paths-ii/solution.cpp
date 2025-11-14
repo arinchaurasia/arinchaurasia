d246fb90adb8731cebd8a51924948c43c2a9ef95
Arin Chaurasia <atulchaurasia39@gmail.com>
2025-11-14 08:34:48 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 12 MB (32.18%)

--- leetcode-solutions/0063-unique-paths-ii/solution.cpp
+++ leetcode-solutions/0063-unique-paths-ii/solution.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-   int countPaths(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp) {
-    if (grid[i][j] == 1) return 0;
-    if (i == 0 && j == 0) return 1; //first position
-    if (i < 0 || j < 0) return 0;
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<long long>> dp(m + 1, vector<long long>(n + 1, 0));
 
-    if (dp[i][j] != -1) return dp[i][j];
+        if (grid[0][0] == 1) return 0;
+        if (grid[m-1][n-1] == 1) return 0;
 
-    int up = 0, left = 0;
-    if (i > 0) up = countPaths(i - 1, j, grid, dp);
-    if (j > 0) left = countPaths(i, j - 1, grid, dp);
-
-    return dp[i][j] = up + left;
-}
-
-int uniquePathsWithObstacles(vector<vector<int>> &grid) {
-    int m = grid.size();
-    int n = grid[0].size();
-    vector<vector<int>> dp(m, vector<int>(n, -1));
-
-    if (grid[0][0] == 1)
-        return 0;
-
-    return countPaths(m - 1, n - 1, grid, dp);
-}
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i == m - 1 && j == n - 1) {
+                    dp[i][j] = 1;
+                } else if (grid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
 };
