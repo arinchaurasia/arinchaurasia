9187eab7421643a04e19c9b0e0c2a6564b716e37
Arin Chaurasia <atulchaurasia39@gmail.com>
2025-11-14 08:53:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.4 MB (26.46%)

--- leetcode-solutions/0120-triangle/solution.cpp
+++ leetcode-solutions/0120-triangle/solution.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int helper(int i,int j,vector<vector<int>>& triangle, vector<vector<int>> &dp){
-        if(i == triangle.size()-1) return triangle[i][j]; 
-        if(dp[i][j] != INT_MIN) {
-            return dp[i][j];
-    }
-    int down = helper(i+1,j,triangle,dp);
-    int diagonal = helper(i+1,j+1,triangle,dp);
-    return dp[i][j] = triangle[i][j]+min(down,diagonal);
-    }
-
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,INT_MIN));
-        return helper(0,0,triangle,dp);
+        
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int j = 0; j < triangle[n-1].size(); j++) {
+            dp[n-1][j] = triangle[n-1][j];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle[i].size(); j++) {
+                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+
+        return dp[0][0];
     }
 };
+
