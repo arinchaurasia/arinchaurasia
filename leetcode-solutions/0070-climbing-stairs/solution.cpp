971a1420d17fad6e96de8b7351ac06f8cd621438
Arin Chaurasia <atulchaurasia39@gmail.com>
2025-11-14 10:00:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (21.01%)

--- leetcode-solutions/0070-climbing-stairs/solution.cpp
+++ leetcode-solutions/0070-climbing-stairs/solution.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    int helper(int n, vector<int>& dp) {
-        // base cases
-        if (n < 0) return 0;
-        if (n == 0) return 1;
-        if (dp[n] != -1) return dp[n];
-        
-        dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
-
+    int climbStairs(int n) {
+        vector<int> dp(n + 1);
+        if (n == 1 || n == 2)
+            return n;
+        dp[1]=1;
+        dp[2]=2;    
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
         return dp[n];
     }
-
-    int climbStairs(int n) {
-        vector<int> dp(n + 1, -1);
-        return helper(n, dp);
-    }
 };
-
