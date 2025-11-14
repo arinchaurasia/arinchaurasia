d0e2ae16a92762224e430a817ef0feed2b41cb3d
Arin Chaurasia <atulchaurasia39@gmail.com>
2025-11-14 05:31:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.6 MB (65.12%)

--- leetcode-solutions/0198-house-robber/solution.cpp
+++ leetcode-solutions/0198-house-robber/solution.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int helper(vector<int> &nums, int ind,vector<int> &dp){
-        if(ind == nums.size() - 1){
-            return nums[ind];
-        } else if(ind >= nums.size()){
-            return 0;
-        }
-        if(dp[ind] != -1) return dp[ind];
-            return dp[ind] = max(helper(nums, ind+1, dp), nums[ind] + helper(nums, ind + 2, dp));
-    }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size() + 1, -1);
-        return helper(nums, 0, dp);
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        vector<int> dp(n+1, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
+        }
+        return dp[n-1];
     }
 };
