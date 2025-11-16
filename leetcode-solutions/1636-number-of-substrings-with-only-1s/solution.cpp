21b57fd3b2730e812acd8325a492665419e4930f
Arin Chaurasia <atulchaurasia39@gmail.com>
2025-11-16 04:10:13 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 11.6 MB (99.35%)

---
+++ leetcode-solutions/1636-number-of-substrings-with-only-1s/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numSub(string s) {
+        const long long MOD = 1e9 + 7;
+        long long ans = 0, curr = 0;
+
+        for (char c : s) {
+            if (c == '1') {
+                curr++;
+                ans = (ans + curr) % MOD;
+            } else {
+                curr = 0;
+            }
+        }
+        return ans;
+    }
+};
