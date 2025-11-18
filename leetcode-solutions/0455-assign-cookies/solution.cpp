7ae447ad627eb8d4f7e91ae9b5f258a7785b1b7e
Arin Chaurasia <atulchaurasia39@gmail.com>
2025-11-18 10:37:59 +0000

    LeetCode Sync - Runtime - 12 ms (29.16%), Memory - 44.9 MB (12.50%)

---
+++ leetcode-solutions/0455-assign-cookies/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(s.begin(),s.end());
+        sort(g.begin(),g.end());
+
+        int i = 0;
+        int j = 0;
+        int n = g.size();
+        int m = s.size();
+        while(i<n && j<m){
+            if(g[i] <= s[j]){
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+};
