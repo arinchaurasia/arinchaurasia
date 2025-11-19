de795295378c0097e19b0e14002c06c181dc0b29
Arin Chaurasia <atulchaurasia39@gmail.com>
2025-11-19 14:03:05 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 13.5 MB (26.40%)

--- leetcode-solutions/0120-triangle/solution.cpp
+++ leetcode-solutions/0120-triangle/solution.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        
         vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        for (int j = 0; j < triangle[n-1].size(); j++) {
+        for(int j = 0; j < n; j++){
             dp[n-1][j] = triangle[n-1][j];
         }
 
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 0; j < triangle[i].size(); j++) {
-                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+        for(int i = n-2; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                int down = dp[i+1][j];
+                int diagonal = dp[i+1][j+1];
+                dp[i][j] = triangle[i][j] + min(down, diagonal);
             }
         }
 
