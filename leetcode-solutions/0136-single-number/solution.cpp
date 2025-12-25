47890ea0591196edce76367e20747ccee0359be4
Arin Chaurasia <atulchaurasia39@gmail.com>
2025-12-25 17:28:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.8 MB (24.29%)

--- leetcode-solutions/0136-single-number/solution.cpp
+++ leetcode-solutions/0136-single-number/solution.cpp
@@ -1,39 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xor1 = 0;
-        for (int i = 0;i<nums.size();i++) {
-            xor1 = xor1^nums[i];
+        int result = 0;
+        for(int num : nums){
+            result ^= num;
         }
-        return xor1;
+        return result;
     }
-
-    // int singleNumber(vector<int>& nums) {
-    //     unordered_map<int,int>mpp;
-    //     for(int i = 0;i<nums.size();i++){
-    //         mpp[nums[i]]++;
-    //     }
-    //     for(auto it:mpp){
-    //         if(it.second == 1){
-    //             return it.first;
-    //         }
-    //     }
-    //     return 0;
-    // }
-
-
-    // int singleNumber(vector<int>& nums) {
-    //     for (int i = 0; i < nums.size(); i++) {
-    //         int num = nums[i];
-    //         int count = 0;
-    //         for (int j = 0; j < nums.size(); j++) {
-    //             if (nums[j] == num) {
-    //                 count++;
-    //             }
-    //         }
-    //         if (count == 1)
-    //             return num;
-    //     }
-    //     return 0;
-    // }
 };
