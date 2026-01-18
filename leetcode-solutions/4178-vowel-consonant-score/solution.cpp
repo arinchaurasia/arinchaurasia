01fe486a9b638863b65c72011a9984561d138ffb
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-01-18 02:50:32 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 9.2 MB (15.00%)

---
+++ leetcode-solutions/4178-vowel-consonant-score/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int vowelConsonantScore(string s) {
+        int score = 0, v = 0, c = 0;
+        for (char ch : s) {
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' ||
+                    ch == 'u') {
+                    v++;
+                } else {
+                    c++;
+                }
+            }
+        }
+        if (c > 0) {
+            return v / c;
+        }
+        return 0;
+    }
+};
