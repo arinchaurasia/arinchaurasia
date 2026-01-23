a8ff1114d2e94d6674c3ed9472d901bfcc7f153a
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-01-23 18:56:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 30.6 MB (6.05%)

--- leetcode-solutions/0189-rotate-array/solution.cpp
+++ leetcode-solutions/0189-rotate-array/solution.cpp
@@ -1,29 +1,11 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k){
-        int n=nums.size();
-        k = k%n;
-        reverse(nums.begin()+n-k,nums.end());
-        reverse(nums.begin(),nums.begin()+n-k);
-        reverse(nums.begin(),nums.end());
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> temp(n);
+        for (int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];
+        }
+        nums = temp;
     }
-
-    // void rotate(vector<int>& nums, int k){
-    //     int n = nums.size();
-    //     k = k%n;
-    //     // Temp
-    //     vector<int>temp;
-    //     for(int i = n-k;i<n;i++){
-    //         temp.push_back(nums[i]);
-    //     }
-    //     // shifting 
-    //     for(int i = n-k-1;i>=0;i--){
-    //         nums[k+i] = nums[i];
-    //     }
-    //     // put back temp
-    //     for(int i = 0;i<k;i++){
-    //         nums[i] = temp[i];
-    //     }
-    // }
-
 };
