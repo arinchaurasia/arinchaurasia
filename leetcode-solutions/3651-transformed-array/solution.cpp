8ad9a61ab430583bfe5cd45987ff0393c772ace9
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-02-05 18:31:12 +0000

    LeetCode Sync - Runtime - 8 ms (45.66%), Memory - 25.5 MB (93.08%)

---
+++ leetcode-solutions/3651-transformed-array/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                int idx = (i + nums[i]) % n;
+                result[i] = nums[idx];
+            }
+            if (nums[i] < 0) {
+                int idx = (i + nums[i]) % n;
+                if (idx < 0)
+                    idx += n;
+                result[i] = nums[idx];
+            }
+            if (nums[i] == 0) {
+                result[i] = nums[i];
+            }
+        }
+        return result;
+    }
+};
