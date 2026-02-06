7e6dc222930d698131b310491434dea1404e4d8f
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-02-06 11:27:16 +0000

    LeetCode Sync - Runtime - 3 ms (89.91%), Memory - 34.9 MB (92.47%)

---
+++ leetcode-solutions/2090-number-of-ways-to-arrive-at-destination/solution.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        const int MOD = 1e9 + 7;
+
+        vector<vector<pair<int, int>>> adj(n);
+        for (int i = 0; i < roads.size(); i++) {
+            int u = roads[i][0];
+            int v = roads[i][1];
+            int w = roads[i][2];
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
+
+        vector<long long> dist(n, LLONG_MAX);
+        vector<int> ways(n, 0);
+
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>,
+            greater<pair<long long, int>>> pq;
+
+        dist[0] = 0;
+        ways[0] = 1;
+        pq.push({0, 0});
+
+        while (!pq.empty()) {
+            long long dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            if (dis > dist[node])
+                continue;
+
+            for (int j = 0; j < adj[node].size(); j++) {
+                int adjNode = adj[node][j].first;
+                int adjNodewt = adj[node][j].second;
+
+                long long newDist = dis + adjNodewt;
+
+                if (newDist < dist[adjNode]) {
+                    dist[adjNode] = newDist;
+                    ways[adjNode] = ways[node];
+                    pq.push({newDist, adjNode});
+                } else if (newDist == dist[adjNode]) {
+                    ways[adjNode] = (ways[adjNode] + ways[node]) % MOD;
+                }
+            }
+        }
+        return ways[n - 1];
+    }
+};
+
