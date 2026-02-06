259752562c78671acddf387c2450a2f65a6b95da
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-02-06 09:36:25 +0000

    Sync LeetCode submission Runtime - 13 ms (66.20%), Memory - 17.1 MB (38.22%)

--- leetcode-solutions/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/solution.cpp
+++ leetcode-solutions/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/solution.cpp
@@ -1,52 +1,42 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        
-        vector<vector<int>> dist(n, vector<int>(n, 1e8));
-
-        for (int i = 0; i < n; i++) 
-        {
+        vector<vector<int>> dist(n, vector<int>(n, 1e9));
+        for (int i = 0; i < n; i++) {
             dist[i][i] = 0;
         }
-        
-        for (auto &e : edges) {
+        for (auto e : edges) {
             int u = e[0];
             int v = e[1]; 
             int w = e[2];
-            
             dist[u][v] = w;
             dist[v][u] = w;
         }
-        
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
-                        dist[i][j] = dist[i][k] + dist[k][j];
-                    }
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
 
-        int AnsNode = -1;
-        int AnsCount = 1e9;
+       int answer = 0;
+        int minCount = 1e9;
 
         for (int i = 0; i < n; i++) {
             int count = 0;
-            
             for (int j = 0; j < n; j++) {
                 if (i != j && dist[i][j] <= distanceThreshold) {
                     count++;
                 }
             }
 
-            if (count <= AnsCount) {
-                AnsCount = count;
-                AnsNode = i;
+            if (count <= minCount) {
+                minCount = count;
+                answer = i;
             }
         }
 
-        return AnsNode;
+        return answer;
     }
 };
-
