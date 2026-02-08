5711debbbc4dbb19503b647da01aa46c83050da9
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-02-08 05:58:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.5 MB (60.11%)

--- leetcode-solutions/0130-surrounded-regions/solution.cpp
+++ leetcode-solutions/0130-surrounded-regions/solution.cpp
@@ -1,58 +1,78 @@
 class Solution {
 public:
-    void bfs(int row, int col, vector<vector<bool>>& visited,
-             vector<vector<char>>& board,int n, int m) {
+    int r;
+    int c;
 
-        queue<pair<int,int>> q;
-        q.push({row, col});
-        visited[row][col] = true;
+    int row[4] = {1, -1, 0, 0};
+    int col[4] = {0, 0, 1, -1};
 
-        int dr[4] = {-1, 0, 1, 0};
-        int dc[4] = {0, 1, 0, -1};
+    bool valid(int i, int j) { return i >= 0 && i < r && j >= 0 && j < c; }
+
+    void solve(vector<vector<char>>& board) {
+        // Code here
+        int n = board.size();
+        int m = board[0].size();
+        r = n;
+        c = m;
+
+        // Apply BFS Operation from border where 0 is present.
+        // Replace O with T
+
+        queue<pair<int, int>> q;
+        // First row
+        for (int j = 0; j < c; j++) {
+            if (board[0][j] == 'O') {
+                q.push(make_pair(0, j));
+                board[0][j] = 'T';
+            }
+        }
+        // First col
+        for (int i = 1; i < r; i++) {
+            if (board[i][0] == 'O') {
+                q.push(make_pair(i, 0));
+                board[i][0] = 'T';
+            }
+        }
+
+        // Last row
+        for (int j = 1; j < c; j++) {
+            if (board[r - 1][j] == 'O') {
+                q.push(make_pair(r - 1, j));
+                board[r - 1][j] = 'T';
+            }
+        }
+        // Last col
+        for (int i = 1; i < r - 1; i++) {
+            if (board[i][c - 1] == 'O') {
+                q.push(make_pair(i, c - 1));
+                board[i][c - 1] = 'T';
+            }
+        }
 
         while (!q.empty()) {
-            auto [r, c] = q.front();
+            int i = q.front().first;
+            int j = q.front().second;
             q.pop();
 
-            for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
+            for (int k = 0; k < 4; k++) {
+                if (valid(i + row[k], j + col[k]) &&
+                    board[i + row[k]][j + col[k]] == 'O') {
+                    board[i + row[k]][j + col[k]] = 'T';
+                    q.push(make_pair(i + row[k], j + col[k]));
+                }
+            }
+        }
+        // Replace all O with X
+        // Replace all T with O
 
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
-                    board[nr][nc] == 'O' && !visited[nr][nc]) {
-
-                    visited[nr][nc] = true;
-                    q.push({nr, nc});
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == 'T') {
+                    board[i][j] = 'O';
                 }
             }
         }
     }
-
-    void solve(vector<vector<char>>& board) {
-        int n = board.size();
-        int m = board[0].size();
-
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
-        for (int i = 0; i < n; i++) {
-            if (board[i][0] == 'O' && !visited[i][0])
-                bfs(i, 0, visited, board, n, m);
-            if (board[i][m-1] == 'O' && !visited[i][m-1])
-                bfs(i, m-1, visited, board, n, m);
-        }
-
-        for (int j = 0; j < m; j++) {
-            if (board[0][j] == 'O' && !visited[0][j])
-                bfs(0, j, visited, board, n, m);
-            if (board[n-1][j] == 'O' && !visited[n-1][j])
-                bfs(n-1, j, visited, board, n, m);
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!visited[i][j])
-                    board[i][j] = 'X';
-            }
-        }
-    }
 };
-
