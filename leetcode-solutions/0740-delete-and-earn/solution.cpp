3f0db64cea2ef3b19c2f81c2dcda820a43f2107a
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-03-10 08:47:18 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 15.7 MB (76.29%)

---
+++ leetcode-solutions/0740-delete-and-earn/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        int maxNum = 0;
+        for(int x : nums)
+            maxNum = max(maxNum, x);
+
+        vector<int> points(maxNum + 1, 0);
+
+        for(int x : nums)
+            points[x] += x;
+            
+        int prev2 = 0;
+        int prev1 = points[1];
+
+        for(int i = 2; i <= maxNum; i++) {
+            int curr = max(prev1, prev2 + points[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
