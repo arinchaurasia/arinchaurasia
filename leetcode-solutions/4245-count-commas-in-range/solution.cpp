05808dd5a250264b84947c297323fffd8aba0910
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-03-15 03:22:29 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 8.5 MB (51.44%)

---
+++ leetcode-solutions/4245-count-commas-in-range/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        long long res = 0;
+
+        for (long long k = 1, low = 1000; low <= n; k++,low *= 1000) {
+            long long upper = min((long long)n,low*1000-1);
+            long long count = upper -low +1;
+            res += k*count;
+        }
+        return res;
+    }
+};
