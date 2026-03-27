dfc5b59728adbc818eb58fff29dceda600417324
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-03-27 14:50:16 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 31.2 MB (97.64%)

---
+++ leetcode-solutions/3215-matrix-similarity-after-cyclic-shifts/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int rows = mat.size();
+        int cols = mat[0].size();
+        k %= cols;
+        
+        if (k == 0) return true;
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i % 2 == 0) {
+                    if (mat[i][j] != mat[i][(j + k) % cols]) return false;
+                } else {
+                    if (mat[i][j] != mat[i][(j - k + cols) % cols]) return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
