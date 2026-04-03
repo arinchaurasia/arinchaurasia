39e0eb65da35c324c8fcda1228bb887586f3d42b
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-04-03 13:05:25 +0000

    Sync LeetCode submission Runtime - 159 ms (84.69%), Memory - 132.5 MB (14.95%)

--- leetcode-solutions/0204-count-primes/solution.cpp
+++ leetcode-solutions/0204-count-primes/solution.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int countPrimes(int n) {
+        // Sieve of Eratosthenes
+        vector<int> prime(n + 1);
         int count = 0;
-        if (n <= 2)
-            return 0;
 
-        vector<bool> isPrime(n, true);
-        isPrime[0] = isPrime[1] = false;
+        for (int i = 2; i < n; i++) {
+            prime[i] = 1;
+        }
 
-        for (int i = 2; i * i < n; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j < n; j += i) {
-                    isPrime[j] = false;
+        for (int i = 2; i * i <= n; i++) {
+            if (prime[i] == 1) {
+                for (int j = i * i; j <= n; j += i) {
+                    prime[j] = 0;
                 }
             }
         }
-
         for (int i = 2; i < n; i++) {
-            if (isPrime[i])
+            if (prime[i] == 1) {
                 count++;
+            }
         }
         return count;
     }
