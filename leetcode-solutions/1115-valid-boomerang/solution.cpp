4f338dfd1dc1888e5fdce687abc569c40c503af4
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-04-05 11:54:44 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 12.6 MB (38.20%)

---
+++ leetcode-solutions/1115-valid-boomerang/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& points) {
+        int area = 0;
+        int x1 = points[0][0];
+        int y1 = points[0][1];
+        int x2 = points[1][0];
+        int y2 = points[1][1];
+        int x3 = points[2][0];
+        int y3 = points[2][1];
+
+        area = abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+
+        if (area == 0)
+            return false;
+        return true;
+    }
+};
