f4b19e6a5db0e5f0b0d4397b1b9fe5ea634014d2
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-04-08 07:24:50 +0000

    Sync LeetCode submission Runtime - 7 ms (5.39%), Memory - 10.2 MB (23.63%)

--- leetcode-solutions/0125-valid-palindrome/solution.cpp
+++ leetcode-solutions/0125-valid-palindrome/solution.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    bool palindrome(int i, string& s) {
+    bool palindrome(int i, string &s){
         int n = s.length();
-        if (i >= n / 2)
-            return true;
-        if (s[i] != s[n - i - 1])
-            return false;
-        return palindrome(i + 1, s);
+        if(i>=n/2) return true;
+        if(s[i] != s[n-i-1]) return false;
+        return palindrome(i+1,s);
     }
     bool isPalindrome(string s) {
         string clean = "";
-        for (char c : s) {
-            if (isalnum(c)) {
+        for(char c:s){
+            if(isalnum(c)){
                 clean += tolower(c);
             }
         }
-        return palindrome(0, clean);
+        return palindrome(0,clean)  ;                      
     }
 };
-
