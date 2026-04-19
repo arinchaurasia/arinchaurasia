21017b76edf32c6fc40459617ab6289a1de9dff0
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-04-19 03:34:47 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 31.6 MB (25.17%)

---
+++ leetcode-solutions/4284-smallest-stable-index-i/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> PrefixMax(n);
+        vector<int> SuffixMin(n);
+
+        PrefixMax[0] = nums[0];
+        SuffixMin[n - 1] = nums[n - 1];
+
+        for (int i = 1; i < n; i++) {
+            PrefixMax[i] = max(PrefixMax[i - 1], nums[i]);
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            SuffixMin[i] = min(SuffixMin[i + 1], nums[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (PrefixMax[i] - SuffixMin[i] <= k) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
