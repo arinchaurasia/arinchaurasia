7348c4a2ebbb4bd9762bf5dc802ffa45bfeb109b
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-04-26 13:52:31 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 8.8 MB (7.94%)

---
+++ leetcode-solutions/0782-jewels-and-stones/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char> s;
+        
+        for(char c : jewels) {
+            s.insert(c);
+        }
+        
+        int count = 0;
+        
+        for(char c : stones) {
+            if(s.count(c)) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};
