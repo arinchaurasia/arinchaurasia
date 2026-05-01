c71c7a00a07a725129b3822db156bdf9d8ab3ce4
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-05-01 09:16:55 +0000

    LeetCode Sync - Runtime - 39 ms (25.02%), Memory - 34.8 MB (12.05%)

---
+++ leetcode-solutions/0647-palindromic-substrings/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    bool solve(string &s,int i,int j,vector<vector<int>>&dp){
+        if(i>=j){
+            return true;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s[i]==s[j]){
+            return  dp[i][j]=solve(s,i+1,j-1,dp);
+        }
+        return  dp[i][j]=false;
+    }
+public:
+    int countSubstrings(string s) {
+        int n=s.size();
+        int count=0;
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(solve(s,i,j,dp)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
