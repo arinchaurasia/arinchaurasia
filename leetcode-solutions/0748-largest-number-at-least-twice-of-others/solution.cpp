2f94f2618967058d37ca2f400d64163662709ff1
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-05-03 21:37:11 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 13.6 MB (98.11%)

---
+++ leetcode-solutions/0748-largest-number-at-least-twice-of-others/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+        int n = nums.size();
+
+        int maxVal = -1, secondMax = -1, index = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > maxVal) {
+                secondMax = maxVal;
+                maxVal = nums[i];
+                index = i;
+            } else if (nums[i] > secondMax) {
+                secondMax = nums[i];
+            }
+        }
+
+        if (maxVal >= 2 * secondMax) {
+            return index;
+        }
+
+        return -1;
+    }
+};
