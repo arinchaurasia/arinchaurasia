f35fe8292721006bf4feb9a0531bcf008609148f
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-05-04 11:03:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10 MB (94.04%)

--- leetcode-solutions/0048-rotate-image/solution.cpp
+++ leetcode-solutions/0048-rotate-image/solution.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        for(int i = 0;i<n-1;i++){
-            for(int j = i+1;j<n;j++){
+        for(int i = 0;i<n;i++){
+            for(int j = i;j<n;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
@@ -11,20 +11,4 @@ public:
             reverse(matrix[i].begin(),matrix[i].end());
         }
     }
-
-    // void rotate(vector<vector<int>>& matrix) {
-    //     int n = matrix.size();
-    //     vector<vector<int>>ans(n,vector<int>(n));
-    //     for(int i = 0;i<n;i++){
-    //         for(int j = 0;j<n;j++){
-    //             ans[j][n-i-1] = matrix[i][j];
-    //         }
-    //     }
-        
-    //     for(int i = 0;i<n;i++){
-    //         for(int j = 0;j<n;j++){
-    //             matrix[i][j] = ans[i][j];
-    //         }
-    //     }
-    // }
 };
