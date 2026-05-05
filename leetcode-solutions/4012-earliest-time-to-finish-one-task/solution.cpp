9bed5d92d0f0028c374c4197df09313914d5483d
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-05-05 10:20:42 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 36.7 MB (76.50%)

---
+++ leetcode-solutions/4012-earliest-time-to-finish-one-task/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int earliestTime(vector<vector<int>>& tasks) {
+        int ans = INT_MAX;
+
+        for (auto &task : tasks) {
+            int finish = task[0] + task[1];
+            ans = min(ans, finish);
+        }
+
+        return ans;
+    }
+};
