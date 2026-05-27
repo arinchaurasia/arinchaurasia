4ad45380d007448a8b59a7299ba1a4c3710d7cc6
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-05-27 12:32:46 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 8.7 MB (13.35%)

---
+++ leetcode-solutions/4305-check-adjacent-digit-differences/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        for (int i = 0; i < s.length() - 1; i++) {
+            int diff = abs(s[i] - s[i + 1]);
+            
+            if (diff > 2) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
