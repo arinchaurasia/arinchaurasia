15e6e466bf6e339ab66ae29c9d9817fa265ae471
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-06-07 02:59:41 +0000

    LeetCode Sync - Runtime - 18 ms (55.53%), Memory - 33.7 MB (40.41%)

---
+++ leetcode-solutions/4289-valid-binary-strings-with-cost-limit/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+        vector<string>ans;
+
+        void dfs(int idx,int n,int k,int cost,string &cur){
+            if(cost>k){
+                return;
+            }
+            if(idx==n){
+                ans.push_back(cur);
+                return;
+            }
+            cur.push_back('0');
+            dfs(idx+1,n,k,cost,cur);
+            cur.pop_back();
+
+            if(idx == 0 || cur.back() == '0'){
+                cur.push_back('1');
+                dfs(idx+1,n,k,cost+idx,cur);
+                cur.pop_back();
+            }
+        }
+    vector<string> generateValidStrings(int n, int k) {
+        string cur = "";
+        dfs(0,n,k,0,cur);
+        return ans;
+    }
+};
