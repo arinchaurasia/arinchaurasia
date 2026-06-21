2a12b369aab2ab4ae456b5874a75f74c2534f3cd
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-06-21 03:10:54 +0000

    LeetCode Sync - Runtime - 166 ms (60.17%), Memory - 34.5 MB (9.95%)

---
+++ leetcode-solutions/4318-valid-subarrays-with-matching-sum-digits-i/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int firstDigit(long long num) {
+        while (num >= 10) {
+            num /= 10;
+        }
+        return (int)num;
+    }
+    int countValidSubarrays(vector<int>& nums, int x) {
+        int n = nums.size();
+        long long ans = 0;
+        vector<long long> prefix(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+        for (int l = 0; l < n; l++) {
+            for (int r = l; r < n; r++) {
+                long long sum = prefix[r + 1] - prefix[l];
+                if (sum % 10 == x && firstDigit(sum) == x) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
