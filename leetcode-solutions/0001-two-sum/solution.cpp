65421638149d5ac343ab2c94bf636de1988cbc83
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-06-26 17:27:34 +0000

    LeetCode Sync - Runtime - 35 ms (35.04%), Memory - 14.1 MB (71.47%)

--- leetcode-solutions/0001-two-sum/solution.cpp
+++ leetcode-solutions/0001-two-sum/solution.cpp
@@ -1,12 +1,52 @@
 class Solution {
 public:
+// vector<int> twoSum(vector<int>& nums, int target) {
+//     map<int, int> mpp;
+//     int n = nums.size();
+//     for (int i = 0; i < n; i++) {
+//         int num = nums[i];
+//         int moreNeeded = target - num;
+//         if (mpp.find(moreNeeded) != mpp.end()) {
+//             return {mpp[moreNeeded], i};
+//         }
+//         mpp[num] = i;
+//     }
+//     return {-1, -1};
+// }
     vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target)
-                    return {i, j};
+        int n = nums.size();
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                int sum = nums[i]+nums[j];
+                if(sum == target){
+                    return {i,j};
+                }
             }
         }
-        return {};
+        return {-1,-1};
     }
+
+    // vector<int> twoSum(vector<int>& nums, int target) {
+    //     vector<pair<int, int>> v;
+
+    //     for (int i = 0; i < nums.size(); i++)
+    //         v.push_back({nums[i], i});
+
+    //     sort(v.begin(), v.end());
+
+    //     int left = 0;
+    //     int right = v.size() - 1;
+
+    //     while (left < right) {
+    //         int sum = v[left].first + v[right].first;
+
+    //         if (sum == target)
+    //             return {v[left].second, v[right].second};
+    //         else if (sum < target)
+    //             left++;
+    //         else
+    //             right--;
+    //     }
+    //     return {};
+    // }
 };
