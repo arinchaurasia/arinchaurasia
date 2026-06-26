1b6c611e95e99c0441bd17b4104f9c00ff254a4a
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-06-26 11:50:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.6 MB (59.43%)

--- leetcode-solutions/0136-single-number/solution.cpp
+++ leetcode-solutions/0136-single-number/solution.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            int num = nums[i];
-            int count = 0;
-            for (int j = 0; j < nums.size(); j++) {
-                if (nums[j] == num) {
-                    count++;
-                }
-            }
-            if (count == 1)
-                return num;
+        int xor1 = 0;
+        for (int i = 0;i<nums.size();i++) {
+            xor1 = xor1^nums[i];
         }
-        return 0;
+        return xor1;
     }
+
+    // int singleNumber(vector<int>& nums) {
+    //     unordered_map<int,int>mpp;
+    //     for(int i = 0;i<nums.size();i++){
+    //         mpp[nums[i]]++;
+    //     }
+    //     for(auto it:mpp){
+    //         if(it.second == 1){
+    //             return it.first;
+    //         }
+    //     }
+    //     return 0;
+    // }
 
 
     // int singleNumber(vector<int>& nums) {
-    //     int result = 0;
-    //     for (int num : nums) {
-    //         result ^= num;
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         int num = nums[i];
+    //         int count = 0;
+    //         for (int j = 0; j < nums.size(); j++) {
+    //             if (nums[j] == num) {
+    //                 count++;
+    //             }
+    //         }
+    //         if (count == 1)
+    //             return num;
     //     }
-    //     return result;
+    //     return 0;
     // }
 };
