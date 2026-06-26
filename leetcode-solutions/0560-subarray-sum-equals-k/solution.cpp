a58ecf6ab54d5a425d7efd4f928b19370a90d21c
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-06-26 15:55:03 +0000

    Sync LeetCode submission Runtime - 65 ms (31.16%), Memory - 47.7 MB (27.61%)

--- leetcode-solutions/0560-subarray-sum-equals-k/solution.cpp
+++ leetcode-solutions/0560-subarray-sum-equals-k/solution.cpp
@@ -1,45 +1,22 @@
 class Solution {
 public:
     int subarraySum(vector<int>& a, int k) {
-        unordered_map<int,int>mpp;
-        mpp[0] = 1;
-        int preSum = 0,cnt = 0;
-        for(int i  =0;i<a.size();i++){
-            preSum += a[i];
-            int remove = preSum-k;
-            cnt+=mpp[remove];
-            mpp[preSum]+=1;
+        map<long long, int> preSumMap;
+        long long sum = 0;
+        int count = 0;
+
+        preSumMap[0] = 1;      // Empty prefix
+
+        for (int i = 0; i < a.size(); i++) {
+            sum += a[i];
+
+            if (preSumMap.find(sum - k) != preSumMap.end()) {
+                count += preSumMap[sum - k];
+            }
+
+            preSumMap[sum]++;
         }
-        return cnt;
+
+        return count;
     }
-    // int subarraySum(vector<int>& a, int k) {
-    //     int n = a.size();
-    //     int cnt = 0;
-    //     for (int i = 0; i < n; i++) {
-    //         int sum = 0;
-    //         for (int j = i; j < n; j++) {
-    //             sum += a[j];
-    //             if (sum == k)
-    //                 cnt++;
-    //         }
-    //     }
-    //     return cnt;
-    // }
-
-
-    // int subarraySum(vector<int>& a, int k) {
-    //     int n = a.size();
-    //     int cnt = 0;
-    //     for (int i = 0; i < n; i++) {
-    //         for (int j = i; j < n; j++) {
-    //             int sum = 0;
-    //             for (int k = i; k <= j; k++) {
-    //                 sum += a[k];
-    //             }
-    //             if (sum == k)
-    //                 cnt++;
-    //         }
-    //     }
-    //     return cnt;
-    // }
 };
