68e00db97a7546d77789ad8a5a542cdea2dabddb
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-06-27 05:40:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.7 MB (49.46%)

--- leetcode-solutions/0075-sort-colors/solution.cpp
+++ leetcode-solutions/0075-sort-colors/solution.cpp
@@ -1,19 +1,45 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        map<int,int>mp;
-        vector<int>ans;
+    // Dutch National Flag Algorithm
+    // void sortColors(vector<int>& a) {
+    //     int n = a.size();
+    //     int low = 0,mid = 0;
+    //     int high = n-1;
+    //     while(mid<=high){
+    //         if(a[mid] == 0){
+    //             swap(a[low],a[mid]);
+    //             low++;
+    //             mid++;
+    //         }
+    //         else if(a[mid] == 1){
+    //             mid++;
+    //         }
+    //         else {
+    //             swap(a[mid],a[high]);
+    //             high--;
+    //         }
+    //     }
+    // }
 
-        for(int i:nums){
-            mp[i]++;
+    void sortColors(vector<int>& a) {
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        int n = a.size();
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 0)
+                cnt0++;
+            else if (a[i] == 1)
+                cnt1++;
+            else
+                cnt2++;
         }
-        for(auto &it:mp){
-            for(int i = 0;i<it.second;i++){
-                ans.push_back(it.first);
-            }
+        for (int i = 0; i < cnt0; i++) {
+            a[i] = 0;
         }
-        for(int i=0;i<nums.size();i++){
-            nums[i] = ans[i];
+        for (int i = cnt0; i < cnt0 + cnt1; i++) {
+            a[i] = 1;
+        }
+        for (int i = cnt0 + cnt1; i < n; i++) {
+            a[i] = 2;
         }
     }
 };
