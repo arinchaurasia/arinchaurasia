e83c6ff1024350ccaea68dace6f1b915b29f2919
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-07-08 10:50:15 +0000

    LeetCode Sync - Runtime - 0 ms (100.00%), Memory - 26.1 MB (94.30%)

---
+++ leetcode-solutions/0540-single-element-in-a-sorted-array/solution.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        if(nums[0] !=nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        int low= 1,high = n-2;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]!=nums[mid+1] && nums[mid]!= nums[mid-1]) return nums[mid];
+            // we are in left half
+            if((mid%2==1 && nums[mid-1] == nums[mid]) ||
+             mid%2==0 && nums[mid] == nums[mid+1] ){
+                low = mid+1; // eliminate the left half
+            }
+            // we are in right half
+            else{
+                high = mid-1; // eliminate the right half
+            }
+        }
+        return -1;
+    }
+    // int singleNonDuplicate(vector<int>& nums) {
+    //     int n = nums.size();
+    //     if(n==1) return nums[0];
+    //     for(int i = 0;i<n;i++){
+    //         if(i==0){
+    //             if(nums[i] !=nums[i+1]) return nums[i];
+    //         }
+    //         else if(i==n-1){
+    //             if(nums[i] !=nums[i-1]) return nums[i];
+    //         }
+    //         else{
+    //             if((nums[i] != nums[i+1]) && (nums[i]!=nums[i-1])) return nums[i];
+    //         }
+    //     }
+    //     return -1;
+    // }
+};
