135028b9409ebf936d2d12bc1f09549ec4d7c480
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-07-31 06:41:46 +0000

    LeetCode Sync - Runtime - 9 ms (50.81%), Memory - 25.5 MB (63.29%)

---
+++ leetcode-solutions/3276-minimum-number-of-pushes-to-type-word-ii/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> freq(26, 0);
+        int ans = 0;
+        // Count frequency of each character
+        for (char ch : word) {
+            freq[ch - 'a']++;
+        }
+        // Sort frequencies in descending order
+        sort(freq.begin(), freq.end(), greater<int>());
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0)
+                break;
+            ans += freq[i] * (i / 8 + 1);
+        }
+        return ans;
+    }
+};
