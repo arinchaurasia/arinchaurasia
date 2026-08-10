cc2f08ecbcb4c6f1abcc84079d4cddc4d216aa8d
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-08-10 10:03:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.2 MB (64.73%)

---
+++ leetcode-solutions/1538-maximum-points-you-can-obtain-from-cards/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxScore(vector<int>& nums, int k) {
+        int n = nums.size();
+        int lsum = 0, rsum = 0, maxSum = 0;
+        int rindex = n - 1;
+        for (int i = 0; i < k; i++) {
+            lsum = lsum + nums[i];
+            maxSum = lsum;
+        }
+        for (int i = k - 1; i >= 0; i--) {
+            lsum = lsum - nums[i];
+            rsum = rsum + nums[rindex];
+            rindex = rindex - 1;
+            maxSum = max(maxSum,lsum+rsum);
+        }
+        return maxSum;
+    }
+};
